822f038ed199f99e7e03e089080e165e4e0bcf74
BuildTools <unconfigured@null.spigotmc.org>
2024-09-12 20:15:10 +0200

    Initial commit

---
+++ src/main/java/org/surviwal/gamemodes/Gamemodes.java
@@ -0,0 +1,17 @@
+package org.surviwal.gamemodes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gamemodes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
